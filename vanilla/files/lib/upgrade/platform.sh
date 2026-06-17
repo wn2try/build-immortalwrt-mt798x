7dbbb207be07e9c9a3b24651bdb981371b9ba0d6
wn2try <108980673+wn2try@users.noreply.github.com>
2026-06-17 16:48:37 +0800

    add more default packages for vanilla

--- vanilla/files/lib/upgrade/platform.sh
+++ vanilla/files/lib/upgrade/platform.sh
@@ -4,3 +4,16 @@ RAMFS_COPY_BIN='fitblk fit_check_sign'
 platform_do_upgrade() {
 	fit_do_upgrade "$1"
 }
+
+PART_NAME=firmware
+
+platform_check_image() {
+	local board=$(board_name)
+
+	[ "$#" -gt 1 ] && return 1
+
+	fit_check_image "$1"
+	return $?
+
+	return 0
+}
