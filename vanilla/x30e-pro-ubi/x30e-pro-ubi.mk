9ec73e743e59668b5d24c339c3415d5b47850dbe
wn2try <108980673+wn2try@users.noreply.github.com>
2026-06-20 17:25:12 +0800

    add wifi kmods for x30e pro

--- vanilla/x30e-pro-ubi/x30e-pro-ubi.mk
+++ vanilla/x30e-pro-ubi/x30e-pro-ubi.mk
@@ -4,7 +4,8 @@ define Device/ruijie_rg-x30e-pro-ubi
   DEVICE_MODEL := RG-X30E Pro (UBI)
   DEVICE_DTS := mt7981b-ruijie-rg-x30e-pro-ubi
   DEVICE_DTS_DIR := ../dts
-  DEVICE_PACKAGES :=kmod-mtd-rw
+  DEVICE_PACKAGES :=kmod-mt7915e kmod-mt7981-firmware \
+  mt7981-wo-firmware kmod-mtd-rw
   BLOCKSIZE := 128k
   PAGESIZE := 2048
   KERNEL_IN_UBI := 1
